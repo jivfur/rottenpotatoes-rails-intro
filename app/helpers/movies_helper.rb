0058862ba5c9b4cad665aee234ade34c68a0c89c
EC2 Default User <ec2-user@ip-172-31-34-248.us-east-2.compute.internal>
2018-02-27 06:11:43 +0000

    part 1 complete

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,10 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+  
+  def highlight(colum)
+    css={:title => " ", :release_date => " "}
+    css[colum]="hilite"
+    return css
+  end
 end
